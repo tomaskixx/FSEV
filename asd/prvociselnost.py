779cbbb24f62aa58355fc1282881210b45253b51
Tomas <98585692+XDnob@users.noreply.github.com>
2024-11-13 13:10:26 +0100

    asd non important lol

--- asd/prvociselnost.py
+++ asd/prvociselnost.py
@@ -1,7 +1,7 @@
 num = int(input("Zadajte cislo: "))
 for i in range(2,num):
     if (num % i) == 0:
-        print(num, "nieje prvocislo")
+        print(num, "nieje prvocislo.")
         break
 else:
     print(num, "je prvocislo")
