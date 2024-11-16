bf29a8748447a8026a102c74a20daece47a15150
Tomas <98585692+XDnob@users.noreply.github.com>
2024-11-16 16:02:41 +0100

    shit dont work lmao

--- asd/einstein_riddle.py
+++ asd/einstein_riddle.py
@@ -1,31 +1,120 @@
-house_number = [None, None, None, None, None]
+house = [None, None, None, None, None]
 colors_set = ["zlta", "modra", "cervena", "zelena", "biela"]
 nationalities_set = ["Brit", "Sved", "Dan", "Nemec", "Nor"]
 drinks_set = ["caj", "kava", "mlieko", "pivo", "voda"]
 cigars_set = ["Pall Mall", "Dunhill", "Blend", "Blue Master", "Prince"]
 animal_set = ["pes", "vtak", "macka", "ryba", "kon"]
 
-for i in range(len(house_number))
+for i in range(len(house)):
+    house[i] = {
+        "farba" : colors_set[:],
+        "narodnost" : nationalities_set[:],
+        "napoj" : drinks_set[:],
+        "cigareta" : cigars_set[:],
+        "zviera" : animal_set[:]
+    }
+
+#1 Brit žije v červenom dome.
+for i in range(5):
+    if "Brit" not in house[i]["narodnost"]:
+        house[i]["farba"] = [x for x in house[i]["farba"] if x != "cervena"]  
+    if "cervena" not in house[i]["farba"]:
+        house[i]["narodnost"] = [x for x in house[i]["narodnost"] if x != "Brit"] 
+
+#2 Sved ma psa.
+for i in range(5):
+    if "Sved" not in house[i]["narodnost"]:
+        house[i]["zviera"] = [x for x in house[i]["zviera"] if x != "pes"]
+    if "pes" not in house[i]["zviera"]:
+        house[i]["narodnost"] = [x for x in house[i]["narodnost"] if x != "Sved"]
+
+#3 Dan pije caj.
+for i in range(5):
+    if "Dan" not in house[i]["narodnost"]:
+        house[i]["napoj"] = [x for x in house[i]["napoj"] if x != "caj"]
+    if "caj" not in house[i]["zviera"]:
+        house[i]["narodnost"] = [x for x in house[i]["narodnost"] if x != "Dan"]
+
+#4 Zelený dom je hneď vľavo vedľa bieleho domu.
+for i in range(4):  
+    
+    if 'zelena' in house[i]['farba'] and 'biela' in house[i + 1]['farba']:
+        house[i]['farba'] = ['zelena']  
+        house[i + 1]['farba'] = ['biela']  
+
+#5 Majiteľ zeleného domu pije kávu
+for i in range(5):
+    if "zelena" not in house[i]["farba"]:
+        house[i]["napoj"] = [x for x in house[i]["napoj"] if x != "kava"]
+    if "kava" not in house[i]["napoj"]:
+        house[i]["farba"] = [x for x in house[i]["farba"] if x != "zelena"]
+
+#6 Osoba, ktorá fajčí Pall Mall, má vtáka.
+for i in range(5):
+    if "Pall Mall" not in house[i]["cigareta"]:
+        house[i]["zviera"] = [x for x in house[i]["zviera"] if x != "vtak"]
+    if "vtak" not in house[i]["zviera"]:
+        house[i]["cigareta"] = [x for x in house[i]["cigareta"] if x != "Pall Mall"]
+
+#7 Majiteľ žltého domu fajčí Dunhill.
+for i in range(5):
+    if "zlta" not in house[i]["farba"]:
+        house[i]["cigareta"] = [x for x in house[i]["cigareta"] if x != "Dunhill"]
+    if "Dunhill" not in house[i]["cigareta"]:
+        house[i]["farba"] = [x for x in house[i]["farba"] if x != "zlta"]
+
+#8 Osoba žijúca v strednom dome pije mlieko.
+house[2]["napoj"] = ["mlieko"]
+
+#9 Nór býva v prvom dome.
+house[0]["narodnost"] = ["Nor"]
+
+#10 Osoba, ktorá fajčí Blend, býva vedľa osoby, ktorá má mačku.
+for i in range(4):
+    if "Blend" in house[i]["cigareta"]:
+        house[i+1]["zviera"] = [x for x in house[i+1]["zviera"] if x != "macka"]
+    if "macka" in house[i]["zviera"]:
+        house[i+1]["cigareta"] = [x for x in house[i+1]["cigareta"] if x != "Blend"]
+
+#11 Osoba, ktorá má koňa, býva vedľa osoby, ktorá fajčí Dunhill.
+for i in range(4):
+    if "kon" in house[i]["zviera"]:
+        house[i+1]["cigareta"] = [x for x in house[i+1]["cigareta"] if x != "Dunhill"]
+    if "Dunhill" in house[i]["cigareta"]:
+        house[i+1]["zviera"] = [x for x in house[i+1]["zviera"] if x != "kon"]
+
+#12 Osoba, ktorá fajčí Blue Master, pije pivo.
+for i in range(5):
+    if "Blue Master" not in house[i]["cigareta"]:
+        house[i]["napoj"] = [x for x in house[i]["napoj"] if x != "pivo"]
+    if "pivo" not in house[i]["napoj"]:
+        house[i]["cigareta"] = [x for x in house[i]["cigareta"] if x != "Blue Master"]
+
+#13 Nemec fajčí Prince.
+for i in range(5):
+    if "Nemec" not in house[i]["narodnost"]:
+        house[i]["cigareta"] = [x for x in house[i]["cigareta"] if x != "Prince"]
+    if "Prince" not in house[i]["cigareta"]:
+        house[i]["narodnost"] = [x for x in house[i]["narodnost"] if x != "Nemec"]
+
+#14 Nór býva vedľa modrého domu.
+for i in range(4):
+    if "Nor" in house[i]["narodnost"]:
+        house[i+1]["farba"] = [x for x in house[i+1]["farba"] if x != "modra"]
+    if "modra" in house[i]["farba"]:
+        house[i+1]["narodnost"] = [x for x in house[i+1]["narodnost"] if x != "Nor"]
+
+#15 Osoba, ktorá fajčí Blend, má suseda, ktorý pije vodu.
+for i in range(4):
+    if "Blend" in house[i]["cigareta"]:
+        house[i+1]["napoj"] = [x for x in house[i+1]["napoj"] if x != "voda"]
+    if "voda" in house[i]["napoj"]:
+        house[i+1]["cigareta"] = [x for x in house[i+1]["cigareta"] if x != "Blend"]
+
+for i in range(5):
+    if "ryba" not in house[i]["zviera"]:
+        print(house[i]["narodnost"])
 
 
-
-'''
-Brit žije v červenom dome.
-Švéd má psa.
-Dán pije čaj.
-Zelený dom je hneď vľavo vedľa bieleho domu.
-Majiteľ zeleného domu pije kávu.
-Osoba, ktorá fajčí Pall Mall, má vtáka.
-Majiteľ žltého domu fajčí Dunhill.
-Osoba žijúca v strednom dome pije mlieko.
-Nór býva v prvom dome.
-Osoba, ktorá fajčí Blend, býva vedľa osoby, ktorá má mačku.
-Osoba, ktorá má koňa, býva vedľa osoby, ktorá fajčí Dunhill.
-Osoba, ktorá fajčí Blue Master, pije pivo.
-Nemec fajčí Prince.
-Nór býva vedľa modrého domu.
-Osoba, ktorá fajčí Blend, má suseda, ktorý pije vodu.
-'''
-
-
-
+for i in range(5):
+    print(house[i]["zviera"])
--- asd/test.py
+++ asd/test.py
@@ -1,14 +1,132 @@
-def input_error():
-	print("Dvere sú zatvorené! Skúste prosím znovu!")
+# Logic gates implementation using basic functions
+def AND(a, b):
+    return a and b
 
-def input_switches():
-	a = int(input("Spínač A: (1 - zapnutý; 0 - vypnutý) "))
-	b = int(input("Spínač B: (1 - zapnutý; 0 - vypnutý) "))
-	return a, b
+def OR(a, b):
+    return a or b
 
-print("Dvere sa otvoria iba vtedy, ak je jeden zo spínačov aktívny, ale nie oba.")
-switchA, switchB = input_switches()
-while switchA == switchB:
-	input_error()
-	switchA, switchB = input_switches()
-print("Otvorili ste štvrté dvere. Gratulujem! Týmto ste prešli ste všetkými úrovňami!")
+def NOT(a):
+    return not a
+
+def XOR(a, b):
+    return a != b
+
+def generate_permutations(elements):
+    """
+    Generate all permutations of a list manually using recursion.
+    """
+    if len(elements) <= 1:
+        return [elements]
+    permutations = []
+    for i in range(len(elements)):
+        rest = elements[:i] + elements[i+1:]
+        for p in generate_permutations(rest):
+            permutations.append([elements[i]] + p)
+    return permutations
+
+def check_conditions(houses):
+    """
+    Check the conditions of the riddle based on the state of the houses.
+    """
+    for house in houses:
+        if house['color'] == 'green':
+            # The green house must be immediately to the left of the white house
+            green_index = next((i for i, h in enumerate(houses) if h['color'] == 'green'), None)
+            white_index = next((i for i, h in enumerate(houses) if h['color'] == 'white'), None)
+            if white_index is None or NOT(AND(green_index is not None, green_index + 1 == white_index)):
+                return False
+        if house['nationality'] == 'Brit':
+            # The Brit lives in the red house
+            if house['color'] != 'red':
+                return False
+        if house['nationality'] == 'Swede':
+            # The Swede keeps dogs as pets
+            if house['pet'] != 'dog':
+                return False
+        if house['nationality'] == 'Dane':
+            # The Dane drinks tea
+            if house['drink'] != 'tea':
+                return False
+        if house['color'] == 'yellow':
+            # The yellow house's owner smokes Dunhill
+            if house['cigar'] != 'Dunhill':
+                return False
+        if house['drink'] == 'milk':
+            # Milk is drunk in the middle house
+            if houses.index(house) != 2:
+                return False
+        if house['nationality'] == 'Norwegian':
+            # The Norwegian lives in the first house
+            if houses.index(house) != 0:
+                return False
+    
+    # The man who smokes Blend lives next to the one who keeps cats
+    blend_index = next((i for i, h in enumerate(houses) if h['cigar'] == 'Blend'), None)
+    cat_index = next((i for i, h in enumerate(houses) if h['pet'] == 'cat'), None)
+    if blend_index is not None and cat_index is not None:
+        if NOT(OR(AND(cat_index == blend_index - 1, cat_index >= 0), AND(cat_index == blend_index + 1, cat_index < 5))):
+            return False
+    
+    # The man who keeps horses lives next to the man who smokes Dunhill
+    horse_index = next((i for i, h in enumerate(houses) if h['pet'] == 'horse'), None)
+    dunhill_index = next((i for i, h in enumerate(houses) if h['cigar'] == 'Dunhill'), None)
+    if horse_index is not None and dunhill_index is not None:
+        if NOT(OR(AND(horse_index == dunhill_index - 1, horse_index >= 0), AND(horse_index == dunhill_index + 1, horse_index < 5))):
+            return False
+    
+    # The owner who smokes BlueMaster drinks beer
+    if any(h['cigar'] == 'BlueMaster' and h['drink'] != 'beer' for h in houses):
+        return False
+    
+    # The German smokes Prince
+    if any(h['nationality'] == 'German' and h['cigar'] != 'Prince' for h in houses):
+        return False
+
+    # The Norwegian lives next to the blue house
+    norwegian_index = next((i for i, h in enumerate(houses) if h['nationality'] == 'Norwegian'), None)
+    blue_index = next((i for i, h in enumerate(houses) if h['color'] == 'blue'), None)
+    if norwegian_index is not None and blue_index is not None:
+        if NOT(OR(AND(blue_index == norwegian_index - 1, blue_index >= 0), AND(blue_index == norwegian_index + 1, blue_index < 5))):
+            return False
+
+    # The man who smokes Blend has a neighbor who drinks water
+    water_index = next((i for i, h in enumerate(houses) if h['drink'] == 'water'), None)
+    if blend_index is not None and water_index is not None:
+        if NOT(OR(AND(water_index == blend_index - 1, water_index >= 0), AND(water_index == blend_index + 1, water_index < 5))):
+            return False
+
+    return True
+
+def solve_riddle():
+    # Initialize possible attributes
+    colors = ['red', 'green', 'white', 'yellow', 'blue']
+    nationalities = ['Brit', 'Swede', 'Dane', 'Norwegian', 'German']
+    drinks = ['tea', 'coffee', 'milk', 'beer', 'water']
+    cigars = ['Pall Mall', 'Dunhill', 'Blend', 'BlueMaster', 'Prince']
+    pets = ['dog', 'bird', 'cat', 'fish', 'horse']
+
+    # Generate all permutations manually
+    color_perms = generate_permutations(colors)
+    nationality_perms = generate_permutations(nationalities)
+    drink_perms = generate_permutations(drinks)
+    cigar_perms = generate_permutations(cigars)
+    pet_perms = generate_permutations(pets)
+
+    # Iterate over all possible permutations for each attribute
+    for color_perm in color_perms:
+        for nationality_perm in nationality_perms:
+            for drink_perm in drink_perms:
+                for cigar_perm in cigar_perms:
+                    for pet_perm in pet_perms:
+                        houses = [
+                            {'color': color_perm[i], 'nationality': nationality_perm[i], 'drink': drink_perm[i], 'cigar': cigar_perm[i], 'pet': pet_perm[i]}
+                            for i in range(5)
+                        ]
+                        if check_conditions(houses):
+                            for i, house in enumerate(houses):
+                                print(f"House {i + 1}: {house}")
+                            return
+
+
+# Solve the riddle
+solve_riddle()
